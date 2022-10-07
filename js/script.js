d0acffe6c595bfc76f44e3f099d9158028c25c68
Omar-Codess <omartaki8@gmail.com>
2022-10-07 18:27:23 +0200

    aggiunte variabili input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 // INPUT
+// Chiedere all'utente il suo nome
+const userNameInput = document.getElementById("user-name")
+
 // Chiedere all'utente di inserire quanti kilometri vuole percorrere
+const userKm = document.getElementById("kilometers")
+
 // Chiedere all'utente di inserire la sua l'età
+const userAge = document.getElementById("user-age")
+
+// Quando l'utente clicca su submit
+const submitBtn = document.getElementById("submit")
+
+submitBtn.addEventListener("click", function () {
+    
+})
+
 
 // ELABORAZIONE
 // calcolo prezzo del biglietto (kilometri * 0.21)
+
 
 // SE l'età dell'utente è < 18 allora applica sconto 20%
 // ALTRIMENTI SE l'età dell'utente è > 65 allora applica sconto 40%
